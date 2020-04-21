9488595bfe5b81f4ad126881c691663041980cd9
pengwuliang <1097674484@qq.com>
2020-04-21 11:04:51 +0800

    â

--- functional_tests.py
+++ functional_tests.py
@@ -24,23 +24,23 @@ class NewVisitorTest(unittest.TestCase):
 
         #enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        inputbox.send_keys('Buy peacock feathers')
-
-        #hits enter. the page updates and now the page lists "1:......"
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )      
+
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.assertIn(
+            '2:use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         #无论如何都会失败，产生错误信息
         self.fail('Finish the test')
